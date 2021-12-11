ffd4ef3116e54032caa3d67bf7602cb73efcb68b
Mahdi-Heydari <mahdiheydari15879@gmail.com>
2021-12-11 15:43:23 +0330

    Finish styling MiniPalette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -9,19 +9,24 @@ const styles = {
     padding: "0.5rem",
     position: "relative",
     overflow: "hidden",
+    height: "100%",
     "&:hover": {
       cursor: "pointer",
     },
   },
   colors: {
-    backgroundColor: "grey",
+    backgroundColor: "#dae1e4",
+    height: "120px",
+    width: "100%",
+    borderRadius: "5px",
+    overflow: "hidden",
   },
   title: {
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center",
     margin: "0",
-    colors: "black",
+    color: "black",
     paddingTop: "0.5rem",
     fontSize: "1rem",
     position: "relative",
@@ -30,14 +35,29 @@ const styles = {
     marginLeft: "0.5rem",
     fontSize: "1.5rem",
   },
+  miniColor: {
+    height: "25%",
+    width: "20%",
+    display: "inline-block",
+    margin: "0 auto",
+    position: "relative",
+    marginBottom: "-3.5px",
+  },
 };
 
 function MiniPalette(props) {
-  const { classes, paletteName, emoji } = props;
+  const { classes, paletteName, emoji, colors } = props;
+  const miniColorBoxes = colors.map((color) => (
+    <div
+      className={classes.miniColor}
+      style={{ backgroundColor: color.color }}
+      key={color.name}
+    ></div>
+  ));
 
   return (
     <div className={classes.root}>
-      <div className={classes.colors}></div>
+      <div className={classes.colors}>{miniColorBoxes}</div>
       <h5 className={classes.title}>
         {paletteName} <span className={classes.emoji}>{emoji}</span>
       </h5>
