76c69f95018d2db1e1f5fdf1007d955e3ae7b7c1
Mahdi-Heydari <mahdiheydari15879@gmail.com>
2021-12-11 15:00:34 +0330

    Start styling MiniPalette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -8,12 +8,9 @@ class PaletteList extends Component {
 
     return (
       <div>
-        <MiniPalette />
         <h1>React Colors</h1>
         {palettes.map((palette) => (
-          <p>
-            <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
-          </p>
+          <MiniPalette {...palette} />
         ))}
       </div>
     );
