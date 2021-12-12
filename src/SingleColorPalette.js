9d5736295ff8065de7bc70083ef970815973f482
Mahdi-Heydari <mahdiheydari15879@gmail.com>
2021-12-12 11:12:48 +0330

    Move Out Styles

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -4,47 +4,7 @@ import { withStyles } from "@material-ui/core/styles";
 import ColorBox from "./ColorBox";
 import PaletteFooter from "./PaletteFooter";
 import Navbar from "./Navbar";
-
-const styles = {
-  Palette: {
-    height: "100vh",
-    display: "flex",
-    flexDirection: "column",
-  },
-  colors: {
-    height: "90%",
-  },
-  goBack: {
-    width: "20%",
-    height: "50%",
-    margin: "0 auto",
-    display: "inline-block",
-    position: "relative",
-    cursor: "pointer",
-    marginBottom: "-4px",
-    opacity: "1",
-    backgroundColor: "black",
-    "& a": {
-      width: "100px",
-      height: "30px",
-      position: "absolute",
-      top: "50%",
-      left: "50%",
-      marginLeft: "-50px",
-      marginTop: "-15px",
-      display: "inline-block",
-      textAlign: "center",
-      outline: "none",
-      backgroundColor: "rgba(255, 255, 255, 0.3)",
-      fontSize: "1rem",
-      lineHeight: "30px",
-      color: "white",
-      textTransform: "uppercase",
-      border: "none",
-      textDecoration: "none",
-    },
-  },
-};
+import styles from "./styles/PaletteStyles";
 
 class SingleColorPalette extends Component {
   constructor(props) {
---
+++ src/styles/PaletteListStyles.js
@@ -0,0 +1,30 @@
+export default {
+  root: {
+    backgroundColor: "blue",
+    height: "100vh",
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "center",
+  },
+  container: {
+    width: "50%",
+    display: "flex",
+    alignItems: "flex-start",
+    flexDirection: "column",
+    flexWrap: "wrap",
+  },
+  nav: {
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    color: "white",
+  },
+  palettes: {
+    boxSizing: "border-box",
+    width: "100%",
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 30%)",
+    gridGap: "5%",
+  },
+};
